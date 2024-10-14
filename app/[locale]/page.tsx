429604eb46d4510982e58c3fb585c48f270d342d
kayzo1903 <kaykinangukammari@gmail.com>
2024-10-14 19:02:34 +0300

    remove usetranslation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,7 @@
-import { useTranslations } from "next-intl";
-import { Link } from "../../i18n/routing";
 import Header from "../components/Header/HomeHeader";
 import Hero from "../components/Hero/Hero";
 
 export default function HomePage() {
-  const t = useTranslations("HomePage");
   return (
     <main className="max-w-screen-2xl mx-auto">
       <div className="mx-auto ">
